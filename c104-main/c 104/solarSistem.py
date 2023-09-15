9168828e6e1aa21ac89dbee278215d1d7ca16083
OttoDragon <103608368+OttoDragon@users.noreply.github.com>
2023-09-15 17:39:31 -0300

    Add files via upload

--- c104-main/c 104/solarSistem.py
+++ c104-main/c 104/solarSistem.py
@@ -2,7 +2,7 @@ import cv2
 
 img = cv2.imread('solar-system.jpg')
 
-
+cv2.waitKey(0)
 cv2.putText(img, 
             "Sol", 
             (100, 80), 
@@ -11,5 +11,4 @@ cv2.putText(img,
             (0, 0, 255), 
             )
 cv2.imshow("solar system", img)
-
-cv2.waitKey(0)
+#prof, quando eu testo a imagem ela pisca e depois sai, mesmo com o waitKey sendo 0.... não consigo terminar o projeto assim
